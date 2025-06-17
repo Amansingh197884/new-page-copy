88c1d9cc1782f10264466c036da99fa4df4f95fd
AMAN <ta5638576@gmail.com>
2025-06-17 18:29:54 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+var nextBtn = document.querySelector('.next'),
+prevBtn = document.querySelector('.prev'),
+carousel = document.querySelector('.carousel'),
+list = document.querySelector('.list'),
+item = document.querySelectorAll('.item'),
+runningTime = document.querySelector('timeRunning')
+
+
+let timeRunning = 3000
+let timeAutoNext =7000
+
+nextBtn.onclick = function(){
+    showSlider('next')
+}
+
+prevBtn.onclick = function(){
+    showSlider('prev')
+}
+
+let runTimeout
+let runNextAuto = setTimeout(
+    () => {
+        nextBtn.click()
+    }, timeAutoNext
+)
+
+function showSlider(type) {
+    let sliderItemsDom = list.querySelectorAll('.carousel .list .item')
+    if(type === 'next'){
+        list.appendChild(sliderItemsDom[0])
+        carousel.classList.add('next')
+    } else{
+        list.prepend(sliderItemsDom[sliderItemsDom.length - 1])
+        carousel.classList.add('prev')
+    }
+}
